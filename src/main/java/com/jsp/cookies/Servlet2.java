6d932a6d8002378f805a7cc3dac846c931adbf47
NikitaJadhav14 <jadhavnikita315@gmail.com>
2023-04-12 21:15:38 +0530

    First Commit

---
+++ src/main/java/com/jsp/cookies/Servlet2.java
@@ -0,0 +1,31 @@
+package com.jsp.cookies;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet2")
+public class Servlet2 extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String name = req.getParameter("name");
+
+		Cookie cookies[] = req.getCookies();
+		String cookieName = "";
+		String cookieValue = "";
+		for (Cookie cookie : cookies) {
+			cookieName = cookie.getName();
+			cookieValue = cookie.getValue();
+		}
+
+		PrintWriter printWriter = resp.getWriter();
+		printWriter.write("<html><body><h1>Welcome back to the application " + cookieValue + "</h1></body></html>");
+
+	}
+}
